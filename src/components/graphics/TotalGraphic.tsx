1356b45d955900399468c1da291770e641b1880c
João Victor Rodrigues <joaovi15i.rodrigues@gmail.com>
2022-04-04 22:55:02 -0300

    layout home page

---
+++ src/components/graphics/TotalGraphic.tsx
@@ -0,0 +1,11 @@
+import { Chart, DoughnutController } from 'chart.js';
+
+Chart.register( DoughnutController )
+function TotalGraphic()
+{
+  return (
+    <></>
+  )
+}
+
+export default TotalGraphic
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,72 @@ function Home()
   return (
     <>
       <Header />
-      <div className="">
+      <div className="containerHome">
+        <div className="totalCard">
+          <p>R$ </p>
+          <p>100,00</p>
+        </div>
+        <div className="filterMethods">
+          <div className="itemFiltered">
+            <input type="checkbox" />
+            <p>Nubank</p>
+          </div>
+          <div className="itemFiltered">
+            <input type="checkbox" />
+            <p>C6 Bank</p>
+          </div>
+          <div className="itemFiltered">
+            <input type="checkbox" />
+            <p>Débito</p>
+          </div>
+        </div>
+        <div id="separator"></div>
+        <div className="debitCard">
+
+          <h2>Útimos gastos:</h2>
+          <div className="item">
+            <div>
+              <p id="date">Segunda, 21 de Março</p>
+              <p id="title">Mercado</p>
+              <p id="paymentType">Crédito</p>
+            </div>
+            <p id="value">R$ 10,00</p>
+          </div>
+          <div className="item">
+            <div>
+              <p id="date">Segunda, 21 de Março</p>
+              <p id="title">Mercado</p>
+              <p id="paymentType">Crédito</p>
+            </div>
+            <p id="value">R$ 10,00</p>
+          </div>
+          <div className="item">
+            <div>
+              <p id="date">Segunda, 21 de Março</p>
+              <p id="title">Mercado</p>
+              <p id="paymentType">Crédito</p>
+            </div>
+            <p id="value">R$ 10,00</p>
+          </div>
+          <div className="item">
+            <div>
+              <p id="date">Segunda, 21 de Março</p>
+              <p id="title">Mercado</p>
+              <p id="paymentType">Crédito</p>
+            </div>
+            <p id="value">R$ 10,00</p>
+          </div>
+          <div className="item">
+            <div>
+              <p id="date">Segunda, 21 de Março</p>
+              <p id="title">Mercado</p>
+              <p id="paymentType">Crédito</p>
+            </div>
+            <p id="value">R$ 10,00</p>
+          </div>
+
+
+        </div>
       </div>
       <Footer />
     </>
