48010bd651bf92f58bf52c4bccf432b77ab8311d
Joao Victor <joao15.vi.rodrigues@gmail.com>
2022-07-19 19:14:57 -0300

    Add buttons to singIn and Create account

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,9 @@
+import './button.scss'
+
+function Button({...props})
+{
+  return (
+      <button {...props}></button>
+  )
+}
+export default Button
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,7 @@
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
+import Button from '../../components/button/Button'
+
 import './login.scss'
 
 function Login()
@@ -7,15 +9,36 @@ function Login()
   return (
     <>
       <Header />
-      <div id="login-card">
-        <form action="">
-          <p>E-mail ou CPF</p>
-          <input type="text" />
-          <p>Senha</p>
-          <input type="password" />
-        </form>
-        <p>Esqueci minha senha</p>
-      </div>
+      
+      <div id="card">
+        <div className="login">
+            <div className="card-title">
+              <h3>Acesse</h3>
+            </div>
+          <form action="">
+            <p>E-mail</p>
+            <input type="text" />
+            <p>Senha</p>
+            <input type="password" />
+          <div className='forgot-password'>
+            <a>Esqueci minha senha</a>
+          </div>
+           <Button type="submit">Entrar</Button>
+          </form>
+        </div>
+        <div className="create-account">
+          <h3>Não tem uma conta?</h3>
+          <p>Crie a sua conta clicando no botão abaixo:</p>
+          <Button type="submit">Criar conta</Button>
+        </div>
+        </div>
+            
+
+        
+          
+        
+
+      
       <Footer />
     </>
   )
